25004f3e2009a31496c144aaf8d96800c9f16873
jangjinhyuk <jeww1234@gmail.com>
2024-08-15 16:15:54 +0900

    first

---
+++ src/component/WeatherBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WeatherBox = () => {
+  return (
+    <div className='weather-box'>
+      <h2>제주</h2>
+      <h1>30도 / 999화씨</h1>
+      <h1>맑은 날씨</h1>
+    </div>
+  )
+}
+
+export default WeatherBox
