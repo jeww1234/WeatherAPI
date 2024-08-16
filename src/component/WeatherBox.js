d507bad537a7bc6143bc87ecf17301377fd24aa4
jangjinhyuk <jeww1234@gmail.com>
2024-08-16 15:02:53 +0900

    weather3

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -7,8 +7,8 @@ const WeatherBox = ({weather}) => {
   let hhtemp = Math.floor((stemp * 1.8)+32)
   return (
     <Container className='weather-box'>
-      <h1 className='text'>{weather?.name}</h1>
-      <h2 className='text'>{stemp}&#176;C / {hhtemp}&#176;F</h2>
+      <div className='text'>{weather?.name}</div>
+      <div className='text'>{stemp}&#176;C / {hhtemp}&#176;F</div>
       <p className='text'>{weather?.weather[0].description}</p>
     </Container>
   )
